d162b53dd028060633e9b7932d56fd55cdc4ea98
0xa0 <hyp3rcr4ft@gmail.com>
2023-07-27 23:40:34 +0200

    test json

--- src/views/BlogGeneralView.vue
+++ src/views/BlogGeneralView.vue
@@ -12,7 +12,7 @@
 <script>
 
 
-const response =  fetch('https://vercelapi-0xa00.vercel.app/blog/friede/post.json')
+const response =  fetch('https://vercelapi-0xa00.vercel.app/json/friede/post.json')
   .then(response => response.text())
 // then add it to the DOM with then
   .then(text => console.log(text));
