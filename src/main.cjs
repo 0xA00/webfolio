0c608b0e1ef20c13f53ae973fcb654f9b79c5cf1
0xa0 <hyp3rcr4ft@gmail.com>
2023-07-27 22:36:09 +0200

    cjs

--- src/main.cjs
+++ src/main.cjs
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.cjs'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
