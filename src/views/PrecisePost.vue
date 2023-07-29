032984b2d9134472671dccba9d3b52ba3d449e17
Lengthrequired <hyp3rcr4ft@gmail.com>
2023-07-29 20:07:22 +0200

    changes

--- src/views/PrecisePost.vue
+++ src/views/PrecisePost.vue
@@ -2,10 +2,6 @@
 <div id="POST">
     <h2 id="title">{{$route.params.id}}</h2>
 
-    <div id="contents">
-        {{content}}
-
-    </div>
 
 </div>
 </template>
@@ -22,13 +18,9 @@ let title = route.params.id
 const response = await fetch(`https://0xa0.dev/blog/`+title+`/index.md`)
 
 datsas = await response.text()
+console.log(datsas)
 
-await new Promise(resolve => {
-    setTimeout(() => {
-        content = datsas
-        resolve()
-    }, 1000)
-})
+
 
 
 </script>
