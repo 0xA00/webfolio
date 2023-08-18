7488ce807347537cc34cbba592518505e6d3c988
0xa0 <hyp3rcr4ft@gmail.com>
2023-08-18 15:15:08 +0200

    bon ça veut pas oskour (bouton marche plus)

--- src/App.vue
+++ src/App.vue
@@ -53,9 +53,10 @@
 
     width: 100px;
     height: 50px;
-
-
 }
+
+
+
 
 #app {
     margin-left: 5%;
@@ -110,78 +111,4 @@ if (os.includes("Linux") && !os2.includes("Firefox")){
     document.body.style.backgroundColor = "#121212"
 }
 
-//get localStorage of activated and if it doesn't exist, set it to false and create it
-let activated = localStorage.getItem("activated") || false
-localStorage.setItem("activated", activated)
-
-//if activated is true, launch a function to change the color of the nightlightmode button
-
-
-//function to change the color of the nightlightmode button
-function nightlightmode(){
-    activated = !activated
-    localStorage.setItem("activated", activated)
-    //get the nightlightmode button
-    let nightlightmodebutton = document.getElementsByClassName("nightlightmodebutton")[0]
-    //get the circle inside the nightlightmode button
-    let circle = document.getElementsByClassName("circle")[0]
-    //get the body
-    let body = document.body
-    //if the body is not in nightlightmode
-    if (activated){
-        //change the body color to white
-        body.style.backgroundColor = "#eae9e9"
-        //change the nightlightmode button color to light green
-        nightlightmodebutton.style.backgroundColor = "#40ec57"
-
-        //change the circle color to white
-        circle.style.backgroundColor = "white"
-        //make the circle go the the right smoothly with a transition translation
-        circle.style.transform = "translateX(100%)"
-        //make every text color black
-        let text = document.getElementsByTagName("p")
-        for (let i = 0; i < text.length; i++){
-            text[i].style.color = "black"
-        }
-        //make every h1 color black
-        let h1 = document.getElementsByTagName("h1")
-        for (let i = 0; i < h1.length; i++){
-            h1[i].style.color = "black"
-        }
-        //make every h2 color black
-        let h2 = document.getElementsByTagName("h2")
-        for (let i = 0; i < h2.length; i++){
-            h2[i].style.color = "black"
-        }
-
-    }
-    //if the body is in nightlightmode
-    else{
-        //change the body color to black
-        body.style.backgroundColor = "#16161d"
-        //change the nightlightmode button color to white
-        nightlightmodebutton.style.backgroundColor = "#b9b9b9"
-        //change the circle color to black
-        circle.style.backgroundColor = "white"
-        //make the circle go the the left smoothly with a transition translation
-        circle.style.transform = "translateX(4%)"
-        //make every text color white
-        let text = document.getElementsByTagName("p")
-        for (let i = 0; i < text.length; i++){
-            text[i].style.color = "#E7EEE9F4"
-        }
-        //make every h1 color E7EEE9F4
-        let h1 = document.getElementsByTagName("h1")
-        for (let i = 0; i < h1.length; i++){
-            h1[i].style.color = "#E7EEE9F4"
-        }
-        //make every h2 color E7EEE9F4
-        let h2 = document.getElementsByTagName("h2")
-        for (let i = 0; i < h2.length; i++){
-            h2[i].style.color = "#E7EEE9F4"
-        }
-
-
-    }
-}
 </script>
