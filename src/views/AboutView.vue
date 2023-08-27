ea9945fa642cf49818a5c3f6e71f021617570bcc
0xa0 <hyp3rcr4ft@gmail.com>
2023-08-28 01:44:55 +0200

    changement back

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,34 +1,5 @@
 <template>
   <div class="about">
-
-      <div id="PFPandName" class="DIVglass">
-        <img alt="Profile Picture" src="../assets/BNA.png">
-        <h1>Hi I'm 0xa0 !<img alt="transflag" src="../assets/tf.png" style="width: 50px; height: 50px; margin-left: 10px; margin-right: 10px; margin-bottom: -10px"><span id="pronouns">She/Her</span>️</h1>
-      </div>
-
-      <div id="languages" class="DIVglass">
-          <h2> languages learned</h2>
-        <span class="ICONS">JAVA <font-awesome-icon icon="fa-brands fa-java" /></span>
-        <span class="ICONS">PYTHON <font-awesome-icon icon="fa-brands fa-python" /></span>
-        <span class="ICONS">JAVASCRIPT <font-awesome-icon icon="fa-brands fa-js" /></span>
-        <span class="ICONS">C-language <font-awesome-icon icon="fa-solid fa-c" /></span>
-        <span class="ICONS">VUEJS <font-awesome-icon icon="fa-brands fa-vuejs" /></span>
-      </div>
-
-      <div id="somethingaboutme" class="DIVglass">
-          <h2>Something about me</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
-      </div>
-
-      <div id="socials" class="DIVglass">
-          <span class="ICONS"><a href="https://twitter.com/0xa0_" target="_blank"><font-awesome-icon icon="fa-brands fa-twitter" /></a></span>
-          <span class="ICONS"><a href="=https://steamcommunity.com/id/0xA00/" target="_blank"><font-awesome-icon icon="fa-brands fa-steam" /></a></span>
-          <a class="ICONS" href="https://github.com/0xA00/" target="_blank"><font-awesome-icon icon="fa-brands fa-github" /></a>
-          <a class="ICONS" href="" ><font-awesome-icon icon="fa-brands fa-discord"/></a>
-      </div>
-
-      <KanyeQuote></KanyeQuote>
-      <!--
     <img alt="Profile Picture" src="../assets/BNA.png">
     <h1>Hi I'm 0xa0 !<img alt="transflag" src="../assets/tf.png" style="width: 50px; height: 50px; margin-left: 10px; margin-right: 10px; margin-bottom: -10px"><span id="pronouns">She/Her</span>️</h1>
 
@@ -44,6 +15,8 @@
     <span class="ICONS"><font-awesome-icon icon="fa-brands fa-vuejs" /></span>
 
 
+      <KanyeQuote></KanyeQuote>
+
 
 
     <h2>Available social medias :</h2>
@@ -52,7 +25,7 @@
     <a class="ICONS" href="=https://steamcommunity.com/id/0xA00/" target="_blank"><font-awesome-icon icon="fa-brands fa-steam" /></a>
     <a class="ICONS" href="https://github.com/0xA00/" target="_blank"><font-awesome-icon icon="fa-brands fa-github" /></a>
     <a class="ICONS" href="" ><font-awesome-icon icon="fa-brands fa-discord"/></a>
-    -->
+
   </div>
 
 
