0104b68605ae8d1f37e228489792764b343aba18
0xa0 <hyp3rcr4ft@gmail.com>
2023-09-01 20:04:36 +0200

    test particleJS

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,83 @@
 <template>
 
-    <Particles
+    <vue-particles
             id="tsparticles"
+            :particlesInit="particlesInit"
+            :particlesLoaded="particlesLoaded"
             :options="{
-                                           background: {
-                                             color: {
-                                               value: '#0d47a1'
-                                             }
-                                           },
-                                           fpsLimit: 60,
-                                           interactivity: {
-                                             detectsOn: 'window',
-                                             events: {
-                                               onClick: {
-                                                 enable: true,
-                                                 mode: 'push'
-                                               },
-                                               onHover: {
-                                                 enable: true,
-                                                 mode: 'repulse'
-                                               },
-                                               resize: true
-                                             },
-                                             modes: {
-                                               bubble: {
-                                                 distance: 400,
-                                                 duration: 2,
-                                                 opacity: 0.8,
-                                                 size: 40,
-                                                 speed: 3
-                                               },
-                                               push: {
-                                                 quantity: 4
-                                               },
-                                               repulse: {
-                                                 distance: 200,
-                                                 duration: 0.4
-                                               }
-                                             }
-                                           },
-                                           particles: {
-                                             color: {
-                                               value: '#ffffff'
-                                             },
-                                             links: {
-                                               color: '#ffffff',
-                                               distance: 150,
-                                               enable: true,
-                                               opacity: 0.5,
-                                               width: 1
-                                             },
-                                             collisions: {
-                                               enable: true
-                                             },
-                                             move: {
-                                               direction: 'none',
-                                               enable: true,
-                                               outMode: 'bounce',
-                                               random: false,
-                                               speed: 6,
-                                               straight: false
-                                             },
-                                             number: {
-                                               density: {
-                                                 enable: true,
-                                                 value_area: 800
-                                               },
-                                               value: 80
-                                             },
-                                             opacity: {
-                                               value: 0.5
-                                             },
-                                             shape: {
-                                               type: 'circle'
-                                             },
-                                             size: {
-                                               random: true,
-                                               value: 5
-                                             }
-                                           },
-                                           detectRetina: true
-                                         }"
+                    background: {
+                        color: {
+                            value: '#0d47a1'
+                        }
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: 'push'
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: 'repulse'
+                            },
+                            resize: true
+                        },
+                        modes: {
+                            bubble: {
+                                distance: 400,
+                                duration: 2,
+                                opacity: 0.8,
+                                size: 40
+                            },
+                            push: {
+                                quantity: 4
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4
+                            }
+                        }
+                    },
+                    particles: {
+                        color: {
+                            value: '#ffffff'
+                        },
+                        links: {
+                            color: '#ffffff',
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1
+                        },
+                        move: {
+                            direction: 'none',
+                            enable: true,
+                            outMode: 'bounce',
+                            random: false,
+                            speed: 6,
+                            straight: false
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800
+                            },
+                            value: 80
+                        },
+                        opacity: {
+                            value: 0.5
+                        },
+                        shape: {
+                            type: 'circle'
+                        },
+                        size: {
+                            random: true,
+                            value: 5
+                        }
+                    },
+                    detectRetina: true
+                }"
     />
   <router-view/>
   <nav>
@@ -154,6 +151,17 @@ if (os.includes("Linux") && !os2.includes("Firefox")){
     document.body.style.backgroundColor = "#121212"
 }
 
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
+const particlesInit = async engine => {
+    //await loadFull(engine);
+    await loadSlim(engine);
+};
+
+const particlesLoaded = async container => {
+    console.log("Particles container loaded", container);
+};
 
 
 
