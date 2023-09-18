8b4c230c50495e307b6eb77e04cd306d99b6b676
0xa0 <hyp3rcr4ft@gmail.com>
2023-09-18 23:56:27 +0200

    PERHAPS ??????????? V4  ??????

--- src/views/pkmn.vue
+++ src/views/pkmn.vue
@@ -27,12 +27,10 @@ export default defineComponent({
             meta: [
                 {"http-equiv": "X-UA-Compatible", content: "IE=edge"},
                 {property:'og:image', content: siteData.pkmn},
-                {property:'twitter:site', content:'@0xa0_'},
                 {name:"theme-color", content:"#42b983"},
                 {name:"viewport", content:"width=device-width,initial-scale=1.0"},
-                {name:"description", content:"Pokemon from the pokedex"},
                 {name:"twitter:card", content:"summary_large_image"},
-                {name:"twitter:image:src", content:siteData.pkmn},
+                {name:"twitter:image", content:siteData.pkmn},
 
             ]
         })
