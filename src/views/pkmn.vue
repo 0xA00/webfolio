56f575413fe506759d02a67bc21a4e1fb085b6e0
0xa0 <hyp3rcr4ft@gmail.com>
2023-09-19 22:49:31 +0200

    big change on embed

--- src/views/pkmn.vue
+++ src/views/pkmn.vue
@@ -19,6 +19,12 @@ export default defineComponent({
                 .then(data => {
                     console.log(data.sprites.front_default)
                     return data.sprites.front_default
+                }),
+            description: fetch(`https://pokeapi.co/api/v2/pokemon-species/${route.params.id}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.flavor_text_entries[0].flavor_text)
+                    return data.flavor_text_entries[0].flavor_text
                 })
         })
 
@@ -31,6 +37,18 @@ export default defineComponent({
                 {property:"viewport", content:"width=device-width,initial-scale=1.0"},
                 {property:"twitter:card", content:"summary_large_image"},
                 {property:"twitter:image:src", content:siteData.pkmn},
+                //make it so that the title is the name of the pokemon
+                {property:"twitter:title", content:siteData.title},
+                //change the description to the pokemon's description
+                {property:"twitter:description", content:siteData.description},
+                {property:"twitter:site", content:"@0xa0_"},
+                {property:"twitter:creator", content:"@0xa0_"},
+                {property:"og:title", content:siteData.title},
+                //change image size to 1200x630
+                {property:"og:image:width", content:"1200"},
+                {property:"og:image:height", content:"630"},
+                {property:"og:description", content:siteData.description},
+                {property:"og:url", content:window.location.href},
 
             ]
         })
